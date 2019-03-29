3a4dcf160000cc815b1b72fd382de36fb155d089
alexLitslink <oleksandr.shcherbonos@litslink.com>
2019-03-29 12:42:59 +0200

    UI assets

---
+++ Assets/Scripts/UI/ARTutorialUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ARTemplate.UI
+{
+    public class ARTutorialUI : MonoBehaviour
+    {
+        [SerializeField] GameObject lookAraund;
+        [SerializeField] GameObject tapToStart;
+        [SerializeField] GameObject resetOriginPoint;
+
+        void Start()
+        {
+            Main.Store.arOrigin.originIsSet.Bind(s =>
+            {
+                resetOriginPoint.SetActive(s);
+                lookAraund.SetActive(!s);
+            });
+            Main.Store.arOrigin.markIsVisible.Bind(s =>
+            {
+                lookAraund.SetActive(!s);
+                tapToStart.SetActive(s);
+            });
+        }
+
+        public void OnTapToStartClick()
+        {
+            Main.Store.arOrigin.originIsSet.Value = true;
+        }
+
+        public void OnResetClick()
+        {
+            Main.Store.arOrigin.originIsSet.Value = false;
+        }
+    }
+}
