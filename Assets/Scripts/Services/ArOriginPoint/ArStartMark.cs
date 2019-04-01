02ed39a38f297f20fc66547c917ab69fb287f2f6
alexLitslink <oleksandr.shcherbonos@litslink.com>
2019-04-01 12:23:16 +0300

    StartMark model

--- Assets/Scripts/Services/ArOriginPoint/ArStartMark.cs
+++ Assets/Scripts/Services/ArOriginPoint/ArStartMark.cs
@@ -8,10 +8,5 @@ namespace ARTemplate.Services.ArOriginPoint
   {
         void OnBecameVisible() => Main.Store.arOrigin.markIsVisible.Value = true;
         void OnBecameInvisible() => Main.Store.arOrigin.markIsVisible.Value = false;
-
-        void Start()
-        {
-            Main.Store.arOrigin.position.LazyBind(s => transform.position = s * Main.Store.arOrigin.zoom.Value);
-        }
   }
 }
